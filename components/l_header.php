b38b6ea8b48381a4454ee7b3c59440c0d8a78fa9
gnlaures <gn.laureano@gmail.com>
2020-07-27 13:09:59 -0300

    CSS - HeaderHome + Nav (inicio)

--- components/l_header.php
+++ components/l_header.php
@@ -1,6 +1,6 @@
 <header class="l-headerHome">
 	<img src="media/img/content/home_header_1.jpg" alt="new collection summer dream">
-	<div class="u-container">
+	<div class="u-container u-containerFlex">
 		<h1>new collection summer dream</h1>
 	</div>
 </header>
--- components/l_nav.php
+++ components/l_nav.php
@@ -116,272 +116,279 @@
 
 
 <nav class="l-nav">
-	<div class="l-nav__brand">
-		<a href="home" title="<?php echo $title; ?>">
-			<img src="media/img/assets/brand.png" alt="<?php echo $title; ?>">
-		</a>
-	</div>
-	<div class="l-nav__support">
-		<li class="c-subMenu">
-			<span class="c-subMenu__mask"></span>
-			<a class="c-subMenu__call js-subMenuPrimary">Icons</a>
-			<div class="c-subMenu__content">
-				<ul class="c-listSupport">
-					<li><a href="#"><i>Icon</i><span>(41) 3026-3999</span></a></li>
-					<li><a href="#"><i>Icon</i><span>(41) 98806-9129</span></a></li>
-					<li class="timeSupport">
-						<span class="caption">Atendimento</span>
-						<span class="caption">2º a 6º das 10h às 17h</span>
-					</li>
-					<li>
-						<span class="caption">Mídias Sociais</span>
-						<ul class="c-socialList --default --secondary">
-							<li><a href="#"><?php include("media/img/icons/social/social_facebook.svg");?></a></li>
-							<li><a href="#"><?php include("media/img/icons/social/social_twitter.svg");?></a></li>
-							<li><a href="#"><?php include("media/img/icons/social/social_youtube.svg");?></a></li>
-							<li><a href="#"><?php include("media/img/icons/social/social_instagram.svg");?></a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-		</li>
-	</div>
-	<ul class="l-nav__categoriesMenu">
-		<li class="c-subMenu">
-			<span class="c-subMenu__mask"></span>
-			<a class="c-subMenu__call js-subMenuPrimary">Anéis de formatura</a>
-			<div class="c-subMenu__content">
-				<div class="u-container u-containerFlex">
-					<div class="content__categories">
-						<ul>
-							<h3>Categoria</h3>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-						</ul>
-					</div>
-					<div class="content__figure">
-						<a href="#">
-							<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
-						</a>
-					</div>
-				</div>
-			</div>
-		</li>
-		<li class="c-subMenu">
-			<span class="c-subMenu__mask"></span>
-			<a class="c-subMenu__call js-subMenuPrimary">Infantis</a>
-			<div class="c-subMenu__content">
-				<div class="u-container u-containerFlex">
-					<div class="content__categories">
-						<ul>
-							<h3>Categoria</h3>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-						</ul>
-					</div>
-					<div class="content__figure">
-						<a href="#">
-							<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
-						</a>
-					</div>
-				</div>
-			</div>
-		</li>
-		<li class="c-subMenu">
-			<span class="c-subMenu__mask"></span>
-			<a class="c-subMenu__call js-subMenuPrimary">Anéis</a>
-			<div class="c-subMenu__content">
-				<div class="u-container u-containerFlex">
-					<div class="content__categories">
-						<ul>
-							<h3>Categoria</h3>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-						</ul>
-					</div>
-					<div class="content__figure">
-						<a href="#">
-							<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
-						</a>
-					</div>
-				</div>
-			</div>
-		</li>
-		<li class="c-subMenu">
-			<span class="c-subMenu__mask"></span>
-			<a class="c-subMenu__call js-subMenuPrimary">Conjuntos</a>
-			<div class="c-subMenu__content">
-				<div class="u-container u-containerFlex">
-					<div class="content__categories">
-						<ul>
-							<h3>Categoria</h3>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-						</ul>
-					</div>
-					<div class="content__figure">
-						<a href="#">
-							<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
-						</a>
-					</div>
-				</div>
-			</div>
-		</li>
-		<li class="c-subMenu">
-			<span class="c-subMenu__mask"></span>
-			<a class="c-subMenu__call js-subMenuPrimary">Brincos</a>
-			<div class="c-subMenu__content">
-				<div class="u-container u-containerFlex">
-					<div class="content__categories">
-						<ul>
-							<h3>Categoria</h3>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-						</ul>
-					</div>
-					<div class="content__figure">
-						<a href="#">
-							<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
-						</a>
-					</div>
-				</div>
-			</div>
-		</li>
-		<li class="c-subMenu">
-			<span class="c-subMenu__mask"></span>
-			<a class="c-subMenu__call js-subMenuPrimary">Correntes</a>
-			<div class="c-subMenu__content">
-				<div class="u-container u-containerFlex">
-					<div class="content__categories">
-						<ul>
-							<h3>Categoria</h3>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-							<li><a href="#">SubCategoria</a></li>
-						</ul>
-					</div>
-					<div class="content__figure">
-						<a href="#">
-							<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
-						</a>
-					</div>
-				</div>
-			</div>
-		</li>
-		<li><a>Outlet</a></li>
-	</ul>
-	<div class="l-nav__search">
-		<div class="c-search">
-			<input type="text" placeholder="Que Jóia você procura?">
-			<button></button>
+	<div class="l-nav__primary">
+		<div class="l-nav__brand">
+			<a href="home" title="<?php echo $title; ?>">
+				<img src="media/img/assets/brand.png" alt="<?php echo $title; ?>">
+			</a>
 		</div>
 	</div>
-	<div class="l-nav__userAcc">
-		<div class="userOff">
-			<a href="#">Entre ou cadastre-se</a>
-		</div>
-		<div class="userOn">
-			<li class="c-subMenu">
+	<div class="l-nav__secondary">
+		<div class="l-nav__support">
+			<div class="c-subMenu">
 				<span class="c-subMenu__mask"></span>
-				<a class="c-subMenu__call js-subMenuPrimary">Guilherme Laureano</a>
+				<a class="c-subMenu__call js-subMenuPrimary">
+					<div class="icons">icons</div>
+					<div class="caption">Atendimento Loja Virtua<br>(41) 3026-3999</div>
+				</a>
 				<div class="c-subMenu__content">
-					<ul>
-						<li><a href="#">Meus pedidos</a></li>
-						<li><a href="#">Meu cadastro</a></li>
-						<li><a href="#">Sair</a></li>
+					<ul class="c-listSupport">
+						<li><a href="#"><i>Icon</i><span>(41) 3026-3999</span></a></li>
+						<li><a href="#"><i>Icon</i><span>(41) 98806-9129</span></a></li>
+						<li class="timeSupport">
+							<span class="caption">Atendimento</span>
+							<span class="caption">2º a 6º das 10h às 17h</span>
+						</li>
+						<li>
+							<span class="caption">Mídias Sociais</span>
+							<ul class="c-socialList --default --secondary">
+								<li><a href="#"><?php include("media/img/icons/social/social_facebook.svg");?></a></li>
+								<li><a href="#"><?php include("media/img/icons/social/social_twitter.svg");?></a></li>
+								<li><a href="#"><?php include("media/img/icons/social/social_youtube.svg");?></a></li>
+								<li><a href="#"><?php include("media/img/icons/social/social_instagram.svg");?></a></li>
+							</ul>
+						</li>
 					</ul>
 				</div>
-			</li>
+			</div>
 		</div>
-	</div>
-	<div class="l-nav__cart">
-		<li class="c-subMenu">
-			<span class="c-subMenu__mask"></span>
-			<a class="c-subMenu__call js-subMenuPrimary">Carrinho</a>
-			<div class="c-subMenu__content">
-				<div class="c-cart">
-					<div class="c-cart__close">
-						<span>Fechar</span>
-						<i>X</i>
+		<ul class="l-nav__categoriesMenu">
+			<li class="c-subMenu">
+				<span class="c-subMenu__mask"></span>
+				<a class="c-subMenu__call js-subMenuPrimary">Anéis de formatura</a>
+				<div class="c-subMenu__content">
+					<div class="u-container u-containerFlex">
+						<div class="content__categories">
+							<ul>
+								<h3>Categoria</h3>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+							</ul>
+						</div>
+						<div class="content__figure">
+							<a href="#">
+								<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
+							</a>
+						</div>
 					</div>
-					<div class="c-cart__item">
-						<div class="item__qtf">01</div>
-						<div class="item__title">Anel de prata com brilhantes</div>
-						<div class="item__price">R$ 258,30</div>
-						<div class="item__remove"><i>X</i></div>
+				</div>
+			</li>
+			<li class="c-subMenu">
+				<span class="c-subMenu__mask"></span>
+				<a class="c-subMenu__call js-subMenuPrimary">Infantis</a>
+				<div class="c-subMenu__content">
+					<div class="u-container u-containerFlex">
+						<div class="content__categories">
+							<ul>
+								<h3>Categoria</h3>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+							</ul>
+						</div>
+						<div class="content__figure">
+							<a href="#">
+								<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
+							</a>
+						</div>
 					</div>
-					<div class="c-cart__item">
-						<div class="item__qtf">01</div>
-						<div class="item__title">Anel de prata com brilhantes</div>
-						<div class="item__price">R$ 258,30</div>
-						<div class="item__remove"><i>X</i></div>
+				</div>
+			</li>
+			<li class="c-subMenu">
+				<span class="c-subMenu__mask"></span>
+				<a class="c-subMenu__call js-subMenuPrimary">Anéis</a>
+				<div class="c-subMenu__content">
+					<div class="u-container u-containerFlex">
+						<div class="content__categories">
+							<ul>
+								<h3>Categoria</h3>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+							</ul>
+						</div>
+						<div class="content__figure">
+							<a href="#">
+								<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
+							</a>
+						</div>
 					</div>
-					<div class="c-cart__total">
-						<div class="total__caption">Subtotal</div>
-						<div class="total__price">R$ 516,60</div>
-						<div class="total__btn"><a href="#" class="c-btn">Fechar Compra</a></div>
+				</div>
+			</li>
+			<li class="c-subMenu">
+				<span class="c-subMenu__mask"></span>
+				<a class="c-subMenu__call js-subMenuPrimary">Conjuntos</a>
+				<div class="c-subMenu__content">
+					<div class="u-container u-containerFlex">
+						<div class="content__categories">
+							<ul>
+								<h3>Categoria</h3>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+							</ul>
+						</div>
+						<div class="content__figure">
+							<a href="#">
+								<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="c-subMenu">
+				<span class="c-subMenu__mask"></span>
+				<a class="c-subMenu__call js-subMenuPrimary">Brincos</a>
+				<div class="c-subMenu__content">
+					<div class="u-container u-containerFlex">
+						<div class="content__categories">
+							<ul>
+								<h3>Categoria</h3>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+							</ul>
+						</div>
+						<div class="content__figure">
+							<a href="#">
+								<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="c-subMenu">
+				<span class="c-subMenu__mask"></span>
+				<a class="c-subMenu__call js-subMenuPrimary">Correntes</a>
+				<div class="c-subMenu__content">
+					<div class="u-container u-containerFlex">
+						<div class="content__categories">
+							<ul>
+								<h3>Categoria</h3>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+								<li><a href="#">SubCategoria</a></li>
+							</ul>
+						</div>
+						<div class="content__figure">
+							<a href="#">
+								<img src="media/img/content/menu_categorieBanner.jpg" alt="Vestidos">
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li><a href="#">Outlet</a></li>
+		</ul>
+		<div class="l-nav__search">
+			<div class="c-search">
+				<input type="text" placeholder="Que Jóia você procura?">
+				<button></button>
+			</div>
+		</div>
+		<div class="l-nav__userAcc">
+			<div class="userOff">
+				<a href="#">Entre ou cadastre-se</a>
+			</div>
+			<div class="userOn">
+				<li class="c-subMenu">
+					<span class="c-subMenu__mask"></span>
+					<a class="c-subMenu__call js-subMenuPrimary">Guilherme Laureano</a>
+					<div class="c-subMenu__content">
+						<ul>
+							<li><a href="#">Meus pedidos</a></li>
+							<li><a href="#">Meu cadastro</a></li>
+							<li><a href="#">Sair</a></li>
+						</ul>
+					</div>
+				</li>
+			</div>
+		</div>
+		<div class="l-nav__cart">
+			<div class="c-subMenu">
+				<span class="c-subMenu__mask"></span>
+				<a class="c-subMenu__call js-subMenuPrimary">Carrinho</a>
+				<div class="c-subMenu__content">
+					<div class="c-cart">
+						<div class="c-cart__close">
+							<span>Fechar</span>
+							<i>X</i>
+						</div>
+						<div class="c-cart__item">
+							<div class="item__qtf">01</div>
+							<div class="item__title">Anel de prata com brilhantes</div>
+							<div class="item__price">R$ 258,30</div>
+							<div class="item__remove"><i>X</i></div>
+						</div>
+						<div class="c-cart__item">
+							<div class="item__qtf">01</div>
+							<div class="item__title">Anel de prata com brilhantes</div>
+							<div class="item__price">R$ 258,30</div>
+							<div class="item__remove"><i>X</i></div>
+						</div>
+						<div class="c-cart__total">
+							<div class="total__caption">Subtotal</div>
+							<div class="total__price">R$ 516,60</div>
+							<div class="total__btn"><a href="#" class="c-btn">Fechar Compra</a></div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</li>
+		</div>
 	</div>
 </nav>
