968628c3cf11caf7f21ee4e4f4dc5d115cf2b16b
gnlaures <gn.laureano@gmail.com>
2020-07-30 13:06:48 -0300

    Marcação Footer

--- components/l_footer.php
+++ components/l_footer.php
@@ -6,6 +6,84 @@
 		<div class="l-footer__cover">
 			<img src="media/img/content/footer_woman.png" alt="Prata e Arte">
 		</div>
-		footer menus
+		<div class="l-footer__institutional">
+			<div class="c-toggleList">
+				<div class="c-toggleList__header">
+					<span>Institucional</span>
+				</div>
+				<div class="c-toggleList__content">
+					<ul>
+						<li><a href="#">Conheça a Prata e Arte</a></li>
+						<li><a href="#">Nossas lojas</a></li>
+						<li><a href="#">Venda institucional (B2B)</a></li>
+						<li><a href="#">Índices de qualidade</a></li>
+						<li><a href="#">Trabalhe na Prata e Arte</a></li>
+						<li><a href="#">Alianças de compromisso</a></li>
+						<li><a href="#">Ebook - Berloques</a></li>
+						<li><a href="#">Ebook - Combine Suas Joias</a></li>
+						<li><a href="#">Blog</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="l-footer__help">
+			<div class="c-toggleList">
+				<div class="c-toggleList__header">
+					<span>Ajuda</span>
+				</div>
+				<div class="c-toggleList__content">
+					<ul>
+						<li><a href="#">Central de ajuda</a></li>
+						<li><a href="#">Onde Comprar</a></li>
+						<li><a href="#">Cuidados com as Joias</a></li>
+						<li><a href="#">Gravações das Joias</a></li>
+						<li><a href="#">Perguntas frequentes</a></li>
+						<li><a href="#">Políticas de garantia</a></li>
+						<li><a href="#">Política de entrega</a></li>
+						<li><a href="#">Política de Segurança e Privacidade</a></li>
+						<li><a href="#">Política de Trocas e devoluções</a></li>
+						<li><a href="#">Formas e Condições de pagamentos</a></li>
+					</ul>
+					<div>
+						<ul class="c-listSupport">
+							<li><a href="#"><i><img src="media/img/icons/icon_phone.png" alt="Telefone"></i><span
+											class="caption">(41) 3026-3999</span></a></li>
+							<li><a href="#"><i><img src="media/img/icons/social/social_whatsapp.svg"
+												alt="Whatsapp"></i><span class="caption">(41) 98806-9129</span></a></li>
+							<li class="timeSupport">
+								<span class="caption">Atendimento</span>
+								<span class="caption">2º a 6º das 10h às 17h</span>
+							</li>
+						</ul>
+						<ul class="c-socialList --rounded --secondary">
+							<li><a href="#"><?php include("media/img/icons/social/social_facebook.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_google.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_instagram.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_link.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_linkedin.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_mail.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_twitter.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_whatsapp.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_youtube.svg");?></a></li>
+							<li><a href="#"><?php include("media/img/icons/social/social_share.svg");?></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="l-footer__payments">
+			<div class="c-toggleList">
+				<div class="c-toggleList__header">
+					<span>Formas de pagamento</span>
+				</div>
+				<div class="c-toggleList__content">
+					<img src="media/img/assets/payment.png" alt="Formas de pagamengo">
+					<img src="media/img/assets/secure.png" alt="Site seguro">
+				</div>
+			</div>
+		</div>
 	</div>
 </footer>
+<div class="l-copyright">
+	<p>Beleza em Prata Comercial Ltda. - CNPJ:17.360.000/0001-03 - Rua XV de Novembro, 413 conj: 06 | Curitiba | PR</p>
+</div>
