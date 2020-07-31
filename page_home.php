fffbb2b7548c0136eb144779f141881e7db19d55
gnlaures <gn.laureano@gmail.com>
2020-07-31 00:52:05 -0300

    90% responsivo home

--- page_home.php
+++ page_home.php
@@ -8,6 +8,7 @@
 				<figcaption><a href="#">Conheça <strong>a nova Coleção</strong></a></figcaption>
 			</figure>
 			<div class="s-newCollection__content">
+				<span class="circleContent"></span>
 				<h1>special jewelry for special people</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
 			</div>
@@ -15,7 +16,8 @@
 	</section>
 	<section class="s-featuredHome">
 		<div class="u-container u-containerFlex">
-			<div class="s-featuredHome__figure" style="background: url('media/img/content/home_special_3.png')"></div>
+			<div class="s-featuredHome__figure" style="background: url('media/img/content/home_special_3.png')"><img
+						src="media/img/content/home_special_3.png" class="u-dnone"></div>
 			<div class="s-featuredHome__product">
 				<a href="#" class="product__resume c-smallCardResume --alignRight">
 					<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
@@ -40,7 +42,7 @@
 			<div class="content u-containerFlex">
 				<h3 class="s-featuredCollection__caption">Coleção<strong>Cordões e colares</strong></h3>
 				<figure class="s-featuredCollection__figure"><img src="media/img/content/home_special_2.png" alt="kids also like jewelry"></figure>
-				<h2 class="s-featuredCollection__title">The Art of Being One Made for Two</h2>
+				<div class="s-featuredCollection__title">The Art of Being One Made for Two <img src="media/img/content/product_21.png" alt="kids also like jewelry" class="u-dnone featuredJewerly"></div>
 			</div>
 		</div>
 	</section>
