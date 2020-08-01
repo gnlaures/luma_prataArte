c15affb14ce4c4cc64d5a57b66b73b665645ad5d
gnlaures <gn.laureano@gmail.com>
2020-08-01 14:37:22 -0300

    Marcação HTML - Page Categorias

--- page_categoria.php
+++ page_categoria.php
@@ -1,115 +1,218 @@
-<section class="s-categories u-stickyElement">
-	<div class="u-containerBig u-containerFlex">
-		<div class="s-categories__menu">
-			<ul class="s-categories__tags">
-				<li><a href="#" class="c-btn --invisible --small --primary">Pré Vendas</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Lançamentos</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Pacotes</a></li>
-			</ul>
-			<ul class="s-categories__categories">
-				<li><a href="#" class="c-btn --invisible --small --primary">Ação</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Carta</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Clássico</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Corrida</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Esporte</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Estratégia</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Infantil</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Luta</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Musical</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">RPG</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Simulação</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Tiro</a></li>
-			</ul>
-		</div>
-		<div class="s-categories__search">
-			<div class="c-search">
-				<input type="text" placeholder="Encontre um título">
-			</div>
-		</div>
-	</div>
-</section>
-
-<header class="l-headerProduct">
-	<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-	<div class="u-containerBig"><h1>A Plague Tale: Innocence</h1></div>
-</header>
-
-<main id="page__products" class="l-innerPage">
-	<section class="s-gamePage">
-		<div class="u-containerBig u-containerFlex">
-			<div class="s-gamePage__content">
-				<article class="s-gamePage__article --description">
-					<h2 class="u-dnone">Descrição</h2>
-					<div class="descriptionContent">
-						<figure>
-							<a href="#">
-								<img src="media/img/content/test_game.jpg" alt="Crash Bandicoot 4- It's About Time">
-							</a>
-						</figure>
-						<div>
-							<p>No ano de 1348, Amicia de Rune é uma jovem de ascendência nobre que vive com sua família na zona rural da Aquitânia, região sudoeste da França, que foi invadida pelo exército inglês durante a Guerra dos Cem Anos.</p>
-							<hr>
-							<p><strong>Censura: </strong>18 Anos - Contém Violência Extrema, conteúdo sexual, drogas ilícitas</p>
-							<p><strong>Tamanho: </strong>6.3GB</p>
+<main id="page__categories" class="l-innerPage">
+	<header class="l-headerCategorie">
+		<img src="media/img/content/categoia_banner.jpg" alt="Coleção de Anéis">
+		<h1>Coleção de <strong>Anéis</strong></h1>
+	</header>
+	<section class="s-filters">
+		<div class="u-container">
+			<ul class="filters__menu">
+				<li class="c-subMenu">
+					<span class="c-subMenu__mask"></span>
+					<a class="c-subMenu__call js-subMenuPrimary">Aro</a>
+					<div class="c-subMenu__content">
+						<div class="content__hero">
+							<div class="content__options">
+								<ul>
+									<li><a href="#">ANÉL PRATA FLOR (2)</a></li>
+									<li><a href="#">ESMALTADA PINK (1)</a></li>
+									<li><a href="#">ANÉL PRATA (2)</a></li>
+									<li><a href="#">JOANINHA VERMELHA (1)</a></li>
+									<li><a href="#">AZUL (1)</a></li>
+									<li><a href="#">BRINCO PRATA FLOR (5)</a></li>
+									<li><a href="#">ESMALTADA (1)</a></li>
+									<li><a href="#">VERMELHO (1)</a></li>
+								</ul>
+							</div>
+							<div class="content__products">
+								<h2>Conheça os mais vendidos</h2>
+								<ul>
+									<li><a href="#"><img src="media/img/content/product_15.jpg" alt="Nome do produto"></a></li>
+									<li><a href="#"><img src="media/img/content/product_16.jpg" alt="Nome do produto"></a></li>
+									<li><a href="#"><img src="media/img/content/product_19.jpg" alt="Nome do produto"></a></li>
+								</ul>
+							</div>
 						</div>
 					</div>
-				</article>
-				<article class="s-gamePage__article --video">
-					<h2>Trailer</h2>
-					<div class="c-video">
-						<div class="u-aspectRatio u-aspectRatio--16-9">
-							<iframe src="https://www.youtube.com/embed/CtP6mNeN6yE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</li>
+			</ul>
+		</div>
+	</section>
+	<section class="s-headResults">
+		<ul class="breadCrumb">
+			<li><a href="#">Home</a></li>
+			<li><a href="#" class="is-active">Anéis</a></li>
+		</ul>
+		<form action="#" class="orderBy">
+			<label class="caption" for="#">Ordenar por:</label>
+			<select name="#" id="#">
+				<option value="#">Relevância</option>
+				<option value="#">Menor preço</option>
+				<option value="#">Maior preço</option>
+				<option value="#">Lorem Ipsum</option>
+				<option value="#">Lorem Ipsum</option>
+			</select>
+		</form>
+	</section>
+	<section class="s-gridProducts">
+		<div class="u-container">
+			<ul class="c-responsiveGrid" data-maxw-breaks="780, 400" data-maxw-cols="2, 1" data-cols-init="3">
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
 						</div>
-					</div>
-				</article>
-				<article class="s-gamePage__article --gallery">
-					<h2>Imagens</h2>
-					<ul class="c-responsiveGrid"
-					    data-maxw-breaks="780"
-					    data-maxw-cols="2"
-					    data-cols-init="3">
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-					</ul>
-				</article>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
+						</div>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
+						</div>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
+						</div>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
+						</div>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
+						</div>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
+						</div>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
+						</div>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#" class="c-cardProduct">
+						<div class="c-cardProduct__resume">
+							<div class="c-smallCardResume --withTopDetail">
+								<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+								<div class="c-smallCardResume__price">
+									<span class="times">3x</span>
+									<span class="price">R$ 258,30</span>
+								</div>
+								<a href="#" class="c-btn --green">Compre agora</a>
+							</div>
+						</div>
+						<div class="c-cardProduct__figure">
+							<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+						</div>
+					</a>
+				</li>
+			</ul>
+			<div class="loadMoreProducts">
+				<a href="#">Exibir mais resultados</a>
 			</div>
-			<aside class="s-gamePage__aside">
-				<div class="s-gamePage__article --jogar">
-					<h2>Jogar</h2>
-					<p>Você ainda não possui acesso a este jogo.</p>
-					<a href="#" class="c-btn --primary">Assine Oneflix agora para jogar</a>
-					<a href="#" class="c-btn --primary --stroke">Comprar somente este jogo</a>
-					<p>Já é assinante? <a href="#">Faça login</a></p>
-				</div>
-			</aside>
 		</div>
 	</section>
 </main>
