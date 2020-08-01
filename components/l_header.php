93b0aa048ac4414c4ff06d054ec0b25f19ea244a
gnlaures <gn.laureano@gmail.com>
2020-08-01 19:49:03 -0300

    Details - Home

--- components/l_header.php
+++
@@ -1,6 +0,0 @@
-<header class="l-headerHome">
-	<img src="media/img/content/home_header_1.jpg" alt="new collection summer dream">
-	<div class="u-container u-containerFlex">
-		<h1>new collection summer dream</h1>
-	</div>
-</header>
--- page_home.php
+++ page_home.php
@@ -1,4 +1,9 @@
-<?php include('components/l_header.php'); ?>
+<header class="l-headerHome">
+	<img src="media/img/content/home_header_1.jpg" alt="new collection summer dream">
+	<div class="u-container u-containerFlex">
+		<h1>new collection summer dream</h1>
+	</div>
+</header>
 
 <main id="page__home">
 	<section class="s-newCollection">
@@ -50,6 +55,9 @@
 		</div>
 	</section>
 	<section class="s-lastFeaturedCollection">
+		<span class="detail__handLeft"></span>
+		<span class="detail__handRight"></span>
+		<span class="detail__handCircle"></span>
 		<div class="u-container u-containerFlex">
 			<div class="s-lastFeaturedCollection__description">
 				<h3>Marina Alvarenga</h3>
