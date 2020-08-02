70dec76476880c70e70393fd09c4712dfdf31a43
gnlaures <gn.laureano@gmail.com>
2020-08-02 02:14:41 -0300

    HTML - page__produto

--- page_produto.php
+++ page_produto.php
@@ -1,115 +1,394 @@
-<section class="s-categories u-stickyElement">
-	<div class="u-containerBig u-containerFlex">
-		<div class="s-categories__menu">
-			<ul class="s-categories__tags">
-				<li><a href="#" class="c-btn --invisible --small --primary">Pré Vendas</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Lançamentos</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Pacotes</a></li>
-			</ul>
-			<ul class="s-categories__categories">
-				<li><a href="#" class="c-btn --invisible --small --primary">Ação</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Carta</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Clássico</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Corrida</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Esporte</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Estratégia</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Infantil</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Luta</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Musical</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">RPG</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Simulação</a></li>
-				<li><a href="#" class="c-btn --invisible --small --primary">Tiro</a></li>
-			</ul>
-		</div>
-		<div class="s-categories__search">
-			<div class="c-search">
-				<input type="text" placeholder="Encontre um título">
+<main id="page__produto" class="l-innerPage">
+	<section class="s-headResults">
+		<ul class="breadCrumb">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Anéis</a></li>
+			<li><a href="#" class="is-active">Anel de Prata Envelhecido Trabalhado</a></li>
+		</ul>
+	</section>
+	<section class="s-introProduct">
+		<div class="u-container">
+			<div class="s-introProduct__resume">
+				<h1></h1>
+				<p></p>
+				<ul>
+					<li><p>Material: Prata 925</p></li>
+					<li><p>Peso: 1g</p></li>
+				</ul>
+			</div>
+			<div class="s-introProduct__figure">
+				// product image
+			</div>
+			<div class="s-introProduct__sale">
+				<div class="c-saleProduct">
+					<div class="c-saleProduct__first">
+						<div class="c-saleProduct__ref">Referência <strong>54191</strong></div>
+						<div class="c-saleProduct__price">
+							<div class="priceOld">De R$ 380,00</div>
+							<div class="priceNow">
+								<span class="caption">Por: R$</span>
+								<span class="num">280,00</span>
+							</div>
+							<div class="pricePartial">Em até <strong><span>6x de R$ 46,67</span></strong></div>
+							<div class="priceCash">Ou <strong>R$ 266,00 à vista com 5% de desconto.</strong></div>
+						</div>
+						<div class="c-saleProduct__shopNow">
+							<a href="#" class="c-btn --green">Compre Agora</a>
+							<div class="c-saleProduct__secure">
+								<i>ic</i>
+								<span>Compra 100% segura</span>
+							</div>
+						</div>
+						<div class="c-saleProduct__shopPair">
+							<a href="#" class="c-btn --primary">Compre o par</a>
+						</div>
+						<ul class="c-saleProduct__linksEnd">
+							<li><a href="#">Incluir na <span>Lista de presente</span></a></li>
+							<li><a href="#">Medidor de <span>Anéis</span></a></li>
+						</ul>
+					</div>
+					<div class="c-saleProduct__second">
+						<div class="c-saleProduct__close">X</div>
+						<ul class="c-saleProduct__partsMenu">
+							<li><a href="#" data-part-menu="1">1º Peça</a></li>
+							<li><a href="#" data-part-menu="2">2º Peça</a></li>
+						</ul>
+						<div class="c-saleProduct__partsContent" data-part-menu="1">
+							<div class="c-saleProduct__choice">
+								// sub menu
+							</div>
+							<ul class="c-saleProduct__choiceOptions">
+								<li>
+									<h3>Caixa especial + R$ 12,00</h3>
+									<div class="optionsHero">
+										<div>
+											<input type="checkbox" name="opt1__a">
+											<label for="opt1__a">Sim</label>
+										</div>
+										<div>
+											<input type="checkbox" name="opt1__b">
+											<label for="opt1__b">Não</label>
+										</div>
+									</div>
+								</li>
+								<li>
+									<h3>Flanela Mágica + R$ 5,00</h3>
+									<div class="optionsHero">
+										<div>
+											<input type="checkbox" name="opt2__a">
+											<label for="opt2__a">Sim</label>
+										</div>
+										<div>
+											<input type="checkbox" name="opt2__b">
+											<label for="opt2__b">Não</label>
+										</div>
+									</div>
+								</li>
+								<li>
+									<h3>Flanela Mágica + R$ 5,00</h3>
+									<div class="optionsHero">
+										<div>
+											<input type="checkbox" name="opt3__a">
+											<label for="opt3__a">Sim</label>
+										</div>
+										<div>
+											<input type="checkbox" name="opt3__b">
+											<label for="opt3__b">Não</label>
+										</div>
+									</div>
+								</li>
+							</ul>
+							<div class="c-saleProduct__ringText">
+								<input type="text" placeholder="Escreve aqui a gravação interna">
+							</div>
+						</div>
+						<div class="c-saleProduct__partsContent" data-part-menu="2">
+							<div class="c-saleProduct__choice">
+								// sub menu
+							</div>
+							<ul class="c-saleProduct__choiceOptions">
+								<li>
+									<h3>Caixa especial + R$ 12,00</h3>
+									<div class="optionsHero">
+										<div>
+											<input type="checkbox" name="opt1__a">
+											<label for="opt1__a">Sim</label>
+										</div>
+										<div>
+											<input type="checkbox" name="opt1__b">
+											<label for="opt1__b">Não</label>
+										</div>
+									</div>
+								</li>
+								<li>
+									<h3>Flanela Mágica + R$ 5,00</h3>
+									<div class="optionsHero">
+										<div>
+											<input type="checkbox" name="opt2__a">
+											<label for="opt2__a">Sim</label>
+										</div>
+										<div>
+											<input type="checkbox" name="opt2__b">
+											<label for="opt2__b">Não</label>
+										</div>
+									</div>
+								</li>
+								<li>
+									<h3>Flanela Mágica + R$ 5,00</h3>
+									<div class="optionsHero">
+										<div>
+											<input type="checkbox" name="opt3__a">
+											<label for="opt3__a">Sim</label>
+										</div>
+										<div>
+											<input type="checkbox" name="opt3__b">
+											<label for="opt3__b">Não</label>
+										</div>
+									</div>
+								</li>
+							</ul>
+							<div class="c-saleProduct__ringText">
+								<input type="text" placeholder="Escreve aqui a gravação interna">
+							</div>
+						</div>
+						<div class="c-saleProduct__shopPair">
+							<a href="#" class="c-btn --primary --disabled">Compre o par</a>
+							<div class="c-saleProduct__secure">
+								<i>ic</i>
+								<span>Compra 100% segura</span>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
-
-<header class="l-headerProduct">
-	<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-	<div class="u-containerBig"><h1>A Plague Tale: Innocence</h1></div>
-</header>
-
-<main id="page__products" class="l-innerPage">
-	<section class="s-gamePage">
-		<div class="u-containerBig u-containerFlex">
-			<div class="s-gamePage__content">
-				<article class="s-gamePage__article --description">
-					<h2 class="u-dnone">Descrição</h2>
-					<div class="descriptionContent">
-						<figure>
-							<a href="#">
-								<img src="media/img/content/test_game.jpg" alt="Crash Bandicoot 4- It's About Time">
-							</a>
-						</figure>
-						<div>
-							<p>No ano de 1348, Amicia de Rune é uma jovem de ascendência nobre que vive com sua família na zona rural da Aquitânia, região sudoeste da França, que foi invadida pelo exército inglês durante a Guerra dos Cem Anos.</p>
-							<hr>
-							<p><strong>Censura: </strong>18 Anos - Contém Violência Extrema, conteúdo sexual, drogas ilícitas</p>
-							<p><strong>Tamanho: </strong>6.3GB</p>
-						</div>
+	</section>
+	<section class="s-productCombination">
+		<div class="u-container">
+			<h2><span>Compre</span> esta combinação</h2>
+			<ul>
+				<li>
+					// card product
+				</li>
+				<li>
+					// +
+				</li>
+				<li>
+					// card product
+				</li>
+				<li>
+					// =
+				</li>
+				<li>
+					// card final value
+				</li>
+			</ul>
+		</div>
+	</section>
+	<section class="s-fullBanner">
+		<img src="media/img/content/produto_banner_1.jpg">
+		<div class="c-saleProduct">
+			<div class="c-saleProduct__first">
+				<div class="c-saleProduct__ref">Referência <strong>54191</strong></div>
+				<div class="c-saleProduct__price">
+					<div class="priceOld">De R$ 380,00</div>
+					<div class="priceNow">
+						<span class="caption">Por: R$</span>
+						<span class="num">280,00</span>
 					</div>
-				</article>
-				<article class="s-gamePage__article --video">
-					<h2>Trailer</h2>
-					<div class="c-video">
-						<div class="u-aspectRatio u-aspectRatio--16-9">
-							<iframe src="https://www.youtube.com/embed/CtP6mNeN6yE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-						</div>
+					<div class="pricePartial">Em até <strong><span>6x de R$ 46,67</span></strong></div>
+					<div class="priceCash">Ou <strong>R$ 266,00 à vista com 5% de desconto.</strong></div>
+				</div>
+				<div class="c-saleProduct__shopNow">
+					<a href="#" class="c-btn --green">Compre Agora</a>
+					<div class="c-saleProduct__secure">
+						<i>ic</i>
+						<span>Compra 100% segura</span>
 					</div>
-				</article>
-				<article class="s-gamePage__article --gallery">
-					<h2>Imagens</h2>
-					<ul class="c-responsiveGrid"
-					    data-maxw-breaks="780"
-					    data-maxw-cols="2"
-					    data-cols-init="3">
+				</div>
+				<div class="c-saleProduct__shopPair">
+					<a href="#" class="c-btn --primary">Compre o par</a>
+				</div>
+				<ul class="c-saleProduct__linksEnd">
+					<li><a href="#">Incluir na <span>Lista de presente</span></a></li>
+					<li><a href="#">Medidor de <span>Anéis</span></a></li>
+				</ul>
+			</div>
+			<div class="c-saleProduct__second">
+				<div class="c-saleProduct__close">X</div>
+				<ul class="c-saleProduct__partsMenu">
+					<li><a href="#" data-part-menu="1">1º Peça</a></li>
+					<li><a href="#" data-part-menu="2">2º Peça</a></li>
+				</ul>
+				<div class="c-saleProduct__partsContent" data-part-menu="1">
+					<div class="c-saleProduct__choice">
+						// sub menu
+					</div>
+					<ul class="c-saleProduct__choiceOptions">
 						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
+							<h3>Caixa especial + R$ 12,00</h3>
+							<div class="optionsHero">
+								<div>
+									<input type="checkbox" name="opt1__a">
+									<label for="opt1__a">Sim</label>
+								</div>
+								<div>
+									<input type="checkbox" name="opt1__b">
+									<label for="opt1__b">Não</label>
+								</div>
+							</div>
 						</li>
 						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
+							<h3>Flanela Mágica + R$ 5,00</h3>
+							<div class="optionsHero">
+								<div>
+									<input type="checkbox" name="opt2__a">
+									<label for="opt2__a">Sim</label>
+								</div>
+								<div>
+									<input type="checkbox" name="opt2__b">
+									<label for="opt2__b">Não</label>
+								</div>
+							</div>
 						</li>
 						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
-						</li>
-						<li>
-							<a href="media/img/content/productPage_header.jpg" class="u-aspectRatio u-aspectRatio--1-1" data-fancybox>
-								<img src="media/img/content/productPage_header.jpg" alt="A Plague Tale: Innocence">
-							</a>
+							<h3>Flanela Mágica + R$ 5,00</h3>
+							<div class="optionsHero">
+								<div>
+									<input type="checkbox" name="opt3__a">
+									<label for="opt3__a">Sim</label>
+								</div>
+								<div>
+									<input type="checkbox" name="opt3__b">
+									<label for="opt3__b">Não</label>
+								</div>
+							</div>
 						</li>
 					</ul>
-				</article>
-			</div>
-			<aside class="s-gamePage__aside">
-				<div class="s-gamePage__article --jogar">
-					<h2>Jogar</h2>
-					<p>Você ainda não possui acesso a este jogo.</p>
-					<a href="#" class="c-btn --primary">Assine Oneflix agora para jogar</a>
-					<a href="#" class="c-btn --primary --stroke">Comprar somente este jogo</a>
-					<p>Já é assinante? <a href="#">Faça login</a></p>
+					<div class="c-saleProduct__ringText">
+						<input type="text" placeholder="Escreve aqui a gravação interna">
+					</div>
 				</div>
-			</aside>
+				<div class="c-saleProduct__partsContent" data-part-menu="2">
+					<div class="c-saleProduct__choice">
+						// sub menu
+					</div>
+					<ul class="c-saleProduct__choiceOptions">
+						<li>
+							<h3>Caixa especial + R$ 12,00</h3>
+							<div class="optionsHero">
+								<div>
+									<input type="checkbox" name="opt1__a">
+									<label for="opt1__a">Sim</label>
+								</div>
+								<div>
+									<input type="checkbox" name="opt1__b">
+									<label for="opt1__b">Não</label>
+								</div>
+							</div>
+						</li>
+						<li>
+							<h3>Flanela Mágica + R$ 5,00</h3>
+							<div class="optionsHero">
+								<div>
+									<input type="checkbox" name="opt2__a">
+									<label for="opt2__a">Sim</label>
+								</div>
+								<div>
+									<input type="checkbox" name="opt2__b">
+									<label for="opt2__b">Não</label>
+								</div>
+							</div>
+						</li>
+						<li>
+							<h3>Flanela Mágica + R$ 5,00</h3>
+							<div class="optionsHero">
+								<div>
+									<input type="checkbox" name="opt3__a">
+									<label for="opt3__a">Sim</label>
+								</div>
+								<div>
+									<input type="checkbox" name="opt3__b">
+									<label for="opt3__b">Não</label>
+								</div>
+							</div>
+						</li>
+					</ul>
+					<div class="c-saleProduct__ringText">
+						<input type="text" placeholder="Escreve aqui a gravação interna">
+					</div>
+				</div>
+				<div class="c-saleProduct__shopPair">
+					<a href="#" class="c-btn --primary --disabled">Compre o par</a>
+					<div class="c-saleProduct__secure">
+						<i>ic</i>
+						<span>Compra 100% segura</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="s-fullBanner">
+		<img src="media/img/content/produto_banner_2.jpg">
+	</section>
+	<section class="s-suggestionsProduct">
+		<div class="u-container">
+			<div class="s-suggestionsProduct__grid">
+				<div class="splide__products">
+					<div class="splide__track">
+						<ul class="splide__list">
+							<li class="splide__slide">
+								<a href="#" class="c-cardProduct">
+									<div class="c-cardProduct__resume">
+										<div class="c-smallCardResume --withTopDetail">
+											<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+											<div class="c-smallCardResume__price">
+												<span class="times">3x</span>
+												<span class="price">R$ 258,30</span>
+											</div>
+										</div>
+									</div>
+									<div class="c-cardProduct__figure">
+										<img src="media/img/content/product_22.png" alt="Anel de prata com brilhantes">
+									</div>
+								</a>
+							</li>
+							<li class="splide__slide">
+								<a href="#" class="c-cardProduct">
+									<div class="c-cardProduct__resume">
+										<div class="c-smallCardResume --withTopDetail">
+											<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+											<div class="c-smallCardResume__price">
+												<span class="times">3x</span>
+												<span class="price">R$ 258,30</span>
+											</div>
+										</div>
+									</div>
+									<div class="c-cardProduct__figure">
+										<img src="media/img/content/product_1.png" alt="Anel de prata com brilhantes">
+									</div>
+								</a>
+							</li>
+							<li class="splide__slide">
+								<a href="#" class="c-cardProduct">
+									<div class="c-cardProduct__resume">
+										<div class="c-smallCardResume --withTopDetail">
+											<div class="c-smallCardResume__title"><h2>Anel de prata <strong>com brilhantes</strong></h2></div>
+											<div class="c-smallCardResume__price">
+												<span class="times">3x</span>
+												<span class="price">R$ 258,30</span>
+											</div>
+										</div>
+									</div>
+									<div class="c-cardProduct__figure">
+										<img src="media/img/content/product_2.png" alt="Anel de prata com brilhantes">
+									</div>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 </main>
